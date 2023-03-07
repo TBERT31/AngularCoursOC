060243e0a60b8fe87b8b98d81af2a24ce2c0a869
Thomas Berteau <berteau.thomas@hotmail.fr>
2023-03-07 17:00:43 +0100

    element snap face

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './face-snap/models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+
+
+
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myOtherSnap !: FaceSnap;
+  myLastSnap !: FaceSnap;
+
+  ngOnInit(){
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit',
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+    );
+
+    this.myOtherSnap = new FaceSnap(
+      'Three Rock Mountain',
+      'Un endroit magnifique pour les randonnées.',
+      new Date(),
+      0,
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/280px-Three_Rock_Mountain_Southern_Tor.jpg',
+    );
+
+    this.myLastSnap = new FaceSnap(
+      'Un bon repas',
+      "Mmmh que c'est bon !",
+      new Date(),
+      0,
+      'https://upload.wikimedia.org/wikipedia/commons/2/2b/Le_go%C3%BBter_sur_l%27herbe.jpg',
+    );
+  }
 }
--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
 
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap !: FaceSnap;
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl !: string;
+  buttonText !: string;
+
+  ngOnInit(){
+    this.buttonText = "Oh Snap!"
+  }
+
+  onSnap(){
+    if(this.buttonText === "Oh Snap!"){
+      this.faceSnap.snaps++;
+      this.buttonText = "Oups, unSnap!";
+    }else{
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh Snap!";
+    }
+  }
 }
