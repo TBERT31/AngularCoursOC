a94a0b3dff12c5c49f21861e8ecf191fa7d8c5a8
tbert31 <berteau.thomas@hotmail.fr>
2024-02-25 14:17:45 +0100

    let go go

--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -5,6 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
-    
+export class FaceSnapComponent implements OnInit{
+    title!: string;
+    description!: string;
+    createdDate!: Date;
+    snaps!: number;
+    imageUrl!: string;
+
+    constructor(
+
+    ){}
+
+    ngOnInit(): void {
+      this.title = "Archibald";
+      this.description = "Mon meilleur ami depuis tout petit !";
+      this.createdDate = new Date();
+      this.snaps = 6;
+      this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    }
 }
