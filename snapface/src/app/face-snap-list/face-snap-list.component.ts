13f8465d5f980fcee3c7474a31c234e1dbb98095
tbert31 <berteau.thomas@hotmail.fr>
2024-04-03 21:52:14 +0200

    When you want to emit untill the destruction of component

--- snapface/src/app/face-snap-list/face-snap-list.component.ts
+++ snapface/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
-import { interval } from 'rxjs';
-import { tap, take } from 'rxjs/operators';
+import { Subject, interval } from 'rxjs';
+import { tap, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[];
+
+  private destroy$!: Subject<boolean>;
 
   constructor(
     private faceSnapService: FaceSnapsService,
   ) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapService.getAllFaceSnaps();
 
     interval(1000).pipe(
-      take(1),
+      // take(1),  // When you know the number of emission
+      takeUntil(this.destroy$), // When you know want to emit until the component destruction
       tap(console.log)
     ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 
 }
