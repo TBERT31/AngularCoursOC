1071017019016bf37c910f40e75cc720a1859ee4
tbert31 <berteau.thomas@hotmail.fr>
2024-04-03 22:57:07 +0200

    reactive form

--- snapface/src/app/landing-page/landing-page.component.ts
+++ snapface/src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -21,7 +22,7 @@ export class LandingPageComponent implements OnInit {
     this.router.navigateByUrl('facesnaps');
   }
 
-  onSubmitForm(): void{
-    console.log(this.userEmail);
+  onSubmitForm(form: NgForm): void{
+    console.log(form.value);
   }
 }
