7969d447dbc0aef638cd96588a667c5f6843cd7b
tbert31 <berteau.thomas@hotmail.fr>
2024-04-03 21:44:40 +0200

    create a memory leak

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -14,10 +14,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     interval(500).pipe(
-      take(10),
+      take(20),
       map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
       tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      mergeMap(color => this.getTrainObservable$(color)),
+      switchMap(color => this.getTrainObservable$(color)),
       tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
     ).subscribe();
   }
