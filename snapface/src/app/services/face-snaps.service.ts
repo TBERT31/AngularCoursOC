b8144c496ba3433f569725e39aa57de0605db2d5
tbert31 <berteau.thomas@hotmail.fr>
2024-04-04 09:11:36 +0200

    Http get snap by id

--- snapface/src/app/services/face-snaps.service.ts
+++ snapface/src/app/services/face-snaps.service.ts
@@ -11,51 +11,20 @@ export class FaceSnapsService {
     private http: HttpClient,
   ) { }
 
-  faceSnaps: FaceSnap[] = [
-    {
-      id:1,
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 190,
-      location: "Paris",
-    },
-    {
-      id:2,
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 62,
-      location: "à la montagne", 
-    },
-    {
-      id:3,
-      title: 'Un bon repas',
-      description: 'Mmmh que c\'est bon !',
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 101
-    }
-  ];
+  faceSnaps: FaceSnap[] = [];
 
   getAllFaceSnaps(): Observable<FaceSnap[]> {
     return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
 }
 
-  getFaceSnapById(faceSnapId:number): FaceSnap {
-    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-    if(!faceSnap){
-      throw new Error('FaceSnap not found!');
-    }else{
-      return faceSnap;
-    }
+  getFaceSnapById(faceSnapId:number): Observable<FaceSnap> {
+    return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`);
   }
 
   snapFaceSnapById(faceSnapId:number, snapType: 'snap' | 'unsnap'): void{
-    const faceSnap = this.getFaceSnapById(faceSnapId);
-    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+    // const faceSnap = this.getFaceSnapById(faceSnapId);
+    // snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+
   }
 
   addFaceSnap(formValue: {title: string, description: string, imageUrl: string, location?: string}): void{
